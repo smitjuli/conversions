a369c7a0eb6ea931a1c6ee62125c53b7b0602f12
smitjuli <juliannepsmith@gmail.com>
2021-01-16 14:15:01 -0500

    example9 completed

--- example9/convert.py
+++ example9/convert.py
@@ -10,3 +10,35 @@
 # be wary of the two numbered year and see if that causes any problems in the interactive
 
 # write the output to a file, output.tsv
+#-------------------------------------------------
+
+import datetime
+import csv
+
+input_date_format = '%Y-%m-%d'  #convention to make these uppercase
+output_date_format = "%-d-%b-%y"
+
+with open('input.txt') as f: 
+	rows = f.read().splitlines()[15:]  
+
+with open('output.tsv', 'w') as f:
+    writer = csv.writer(f, delimiter='\t')
+    writer.writerow(['date', 'value']) 
+
+    for row in rows:
+        date, value = line.split() 
+        parsed_date = datetime.datetime.strptime(date, input_date_format)
+        output_date = parsed_date.strftime(output_date_format)
+        
+        if parsed_date.year < 1970:
+            continue
+
+        writer.writerow([output_date, value])
+
+
+
+
+
+
+
+
